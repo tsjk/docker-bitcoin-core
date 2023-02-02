cf970b3e7ecfed063f05cb38f8ace28ebbdeced4
Tamas Jantvik <tsjk@hotmail.com>
2023-02-02 07:34:22 +0100

    Avoiding to resetting permissions during non-daemon-spawning calls

--- 22/alpine/docker-entrypoint.sh
+++ 22/alpine/docker-entrypoint.sh
@@ -13,20 +13,15 @@ echo "$0: assuming uid:gid for bitcoin:bitcoin of $(id -u bitcoin):$(id -g bitco
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for bitcoind"
-
   set -- bitcoind "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
-  mkdir -p "$BITCOIN_DATA"
-  chmod 700 "$BITCOIN_DATA"
-  # Fix permissions for home dir.
-  chown -R bitcoin:bitcoin "$(getent passwd bitcoin | cut -d: -f6)"
-  # Fix permissions for bitcoin data dir.
-  chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+if [ "$1" = "bitcoind" ]; then
+  mkdir -p "$BITCOIN_DATA" && { \
+    chmod 700 "$BITCOIN_DATA"; \
+    chown -R bitcoin "$BITCOIN_DATA"; }
 
   echo "$0: setting data directory to $BITCOIN_DATA"
-
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
 
