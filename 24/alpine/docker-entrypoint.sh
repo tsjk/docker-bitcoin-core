a74d4ee14189b32de94b4f5cb80dc81e264a5749
Tamas Jantvik <tsjk@hotmail.com>
2023-10-01 15:51:28 +0200

    Update of v24 to use custom entrypoint

--- 24/alpine/docker-entrypoint.sh
+++ 24/alpine/docker-entrypoint.sh
@@ -13,27 +13,34 @@ echo "$0: assuming uid:gid for bitcoin:bitcoin of $(id -u bitcoin):$(id -g bitco
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for bitcoind"
-
   set -- bitcoind "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
-  mkdir -p "$BITCOIN_DATA"
-  chmod 700 "$BITCOIN_DATA"
-  # Fix permissions for home dir.
-  chown -R bitcoin:bitcoin "$(getent passwd bitcoin | cut -d: -f6)"
-  # Fix permissions for bitcoin data dir.
-  chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+if [ "$1" = "bitcoind" ]; then
+  mkdir -p "$BITCOIN_DATA" && { \
+    chmod 700 "$BITCOIN_DATA"; \
+    chown -R bitcoin "$BITCOIN_DATA"; }
 
   echo "$0: setting data directory to $BITCOIN_DATA"
-
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
 
 if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
-  echo
-  exec su-exec bitcoin "$@"
+  if [ "$1" = "bitcoind" ]; then
+    [ -z "$TOR_SOCKSD" ] || { [ -e /tmp/socat-tor_socks.lock ] && [ -e /tmp/socat-tor_socks.pid ] && kill -0 `cat /tmp/socat-tor_socks.pid` > /dev/null 2>&1; } || {
+      rm -f /tmp/socat-tor_socks.lock /tmp/socat-tor_socks.pid
+      su -s /bin/sh bitcoin -c "/usr/bin/socat -L /tmp/socat-tor_socks.lock TCP4-LISTEN:9050,bind=127.0.0.1,reuseaddr,fork TCP4:$TOR_SOCKSD" &
+      echo $! > /tmp/socat-tor_socks.pid; }
+    [ -z "$TOR_CTRLD" ] || { [ -e /tmp/socat-tor_ctrl.lock ] && [ -e /tmp/socat-tor_ctrl.pid ] && kill -0 `cat /tmp/socat-tor_ctrl.pid` > /dev/null 2>&1; } || {
+      rm -f /tmp/socat-tor_ctrl.lock /tmp/socat-tor_ctrl.pid
+      su -s /bin/sh bitcoin -c "/usr/bin/socat -L /tmp/socat-tor_ctrl.lock  TCP4-LISTEN:9051,bind=127.0.0.1,reuseaddr,fork TCP4:$TOR_CTRLD" &
+      echo $! > /tmp/socat-tor_ctrl.pid; }
+    echo "$0: launching bitcoind as a background job"; echo
+    p="$1"; shift 1; { su -s /bin/sh bitcoin -c "${BITCOIN_PREFIX}/bin/$p $@" & }
+    wait -n
+  else
+    echo; sudo -u bitcoin -- "$@"
+  fi
+else
+  echo; exec "$@"
 fi
-
-echo
-exec "$@"
